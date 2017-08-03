53cf663b1844b73fbdfd81cf927564c3346f6ee1
debian <debian@localhost.localdomain>
2017-08-03 12:43:20 -0300

    alterado cliente mqtt

--- servidor/mqtt/cliente.py
+++ servidor/mqtt/cliente.py
@@ -2,14 +2,10 @@ import os
 import sys
 import django
 import paho.mqtt.client as mqttClient
-from collections import OrderedDict
 
 sys.path.insert(0, os.path.abspath(os.path.join(__file__ ,"../..")))
 os.environ["DJANGO_SETTINGS_MODULE"] = "servidor.settings"
 django.setup()
-
-from mqtt.models import User
-from ecdsa import VerifyingKey, BadSignatureError
 
 def onConnect(client, userdata, flags, rc):
     print("Connected with result code "+str(rc))
@@ -31,42 +27,17 @@ def onMessage(client, userdata, msg):
 #        print(topico + ": " + packet)
         message = eval(packet)
         print(message)
-        #res, message = verificaAssinatura(usuario, packet)
-        #if(res):
-        #    print(message)
-
     except Exception as e:
         print(e)
 
 def onDisconnect(client, userdata, rc):
     client.reconnect()
-    #print("Até a próxima\n")
-    #exit()
-
-
-def verificaAssinatura(username, packet):
-    u = User.objects.filter(username=username).get()
-    pk = u.publickey
-    vk = VerifyingKey.from_pem(pk)
-    packet = eval(packet)
-    message = packet['message']
-    sig = packet['signature']
-    try:
-        vk.verify(sig, str(message).encode())
-        print("Assinatura válida")
-        return (True, message)
-    except BadSignatureError:
-        print("Assinatura inválida")
-        return (False, None)
-
 
 def conecta():
-    u = User.objects.filter(username="servidor").get()
-    nome = u.username
-    senha = "servidor"
+    nome = 'servidor' 
     
     client = mqttClient.Client(client_id=nome, clean_session=True, userdata="None", protocol="MQTTv311", transport="tcp")
-    client.username_pw_set(username=nome, password=senha)
+    client.username_pw_set(username=nome)
 
     client.on_connect = onConnect
     client.on_message = onMessage
